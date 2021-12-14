00a8ee3e1a4f5360b92c2528ebae0385043b4573
mogooee <mogoo22@naver.com>
2021-12-14 18:39:49 +0900

    feat: remove Menu & Refact

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,31 @@
-//step1 요구사항 구현을 위한 전략
-//TODO 메뉴 추가
-// -[V] 메뉴의 이름을 입력 받고 엔터키 입력으로 메뉴 추가
-// -[V] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴 추가
-// -[V] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// -[V] 총 메뉴 개수를 count하여 상단에 보여준다
-// -[V] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// -[V] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// step1 요구사항 구현을 위한 전략
+// TODO 메뉴 추가
+// - [V] 메뉴의 이름을 입력 받고 엔터키 입력으로 메뉴 추가
+// - [V] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴 추가
+// - [V] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [V] 총 메뉴 개수를 count하여 상단에 보여준다
+// - [V] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [V] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+// TODO 메뉴 수정
+// - [V] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [V] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+// TODO 메뉴 삭제
+// - [V] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [V] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [V] 총 메뉴 개수를 count하여 상단에 보여준다.
 
 //반복되는 코드를 간결하게 나타냄
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // TODO 메뉴 수정
-  // - [V] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-  // - [V] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
-
   // 이벤트 위임
-  $("#espresso-menu-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("menu-edit-button")) {
-      const $menuName = e.target.closest("li").querySelector(".menu-name");
-      const updatedMenuName = prompt(
-        "메뉴명을 수정하세요",
-        $menuName.innerText
-      );
-      $menuName.innerText = updatedMenuName;
-    }
-  });
 
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+  const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+  };
 
   const addMenuName = () => {
     if (!$("#espresso-menu-name").value) {
@@ -61,14 +57,38 @@ function App() {
       menuItemTemplate(espressoMenuName)
     );
 
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-    $(".menu-count").innerText = `총 ${menuCount}개`;
+    updateMenuCount();
     $("#espresso-menu-name").value = "";
   };
 
-  $("#espresso-menu-submit-button").addEventListener("click", (e) => {
-    addMenuName();
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = (e) => {
+    if (confirm("정말 삭제하시겠습니까?")) {
+      e.target.closest("li").remove();
+      updateMenuCount();
+    }
+  };
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      updateMenuName(e);
+    }
+
+    if (e.target.classList.contains("menu-remove-button")) {
+      removeMenuName(e);
+    }
   });
+
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
 
   // 메뉴의 이름을 입력받는 것
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
